45d21f4d50d9b2ddf32231b4db64c21a0fc7f2e1
Nathan Louth <77612558+NathanLouth@users.noreply.github.com>
2021-04-25 13:22:50 +0100

    Minor Updates
    - Fixed tab order
    - Window size larger
    - Text and input boxes are now larger

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -42,77 +42,88 @@ namespace msgGUI
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(10, 8);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(96, 15);
+            this.label1.Size = new System.Drawing.Size(125, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Computer Name";
             // 
             // ComputerNameTxT
             // 
-            this.ComputerNameTxT.Location = new System.Drawing.Point(12, 27);
+            this.ComputerNameTxT.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ComputerNameTxT.Location = new System.Drawing.Point(10, 31);
             this.ComputerNameTxT.Name = "ComputerNameTxT";
-            this.ComputerNameTxT.Size = new System.Drawing.Size(300, 23);
+            this.ComputerNameTxT.Size = new System.Drawing.Size(318, 22);
             this.ComputerNameTxT.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 122);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(10, 104);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 15);
+            this.label2.Size = new System.Drawing.Size(74, 20);
             this.label2.TabIndex = 2;
             this.label2.Text = "Message";
             // 
             // MessageTxT
             // 
-            this.MessageTxT.Location = new System.Drawing.Point(12, 140);
+            this.MessageTxT.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MessageTxT.Location = new System.Drawing.Point(10, 127);
             this.MessageTxT.Name = "MessageTxT";
-            this.MessageTxT.Size = new System.Drawing.Size(300, 96);
-            this.MessageTxT.TabIndex = 4;
+            this.MessageTxT.Size = new System.Drawing.Size(318, 107);
+            this.MessageTxT.TabIndex = 3;
             this.MessageTxT.Text = "";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 66);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 56);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 15);
+            this.label3.Size = new System.Drawing.Size(51, 20);
             this.label3.TabIndex = 5;
             this.label3.Text = "Users";
             // 
             // UsersTxT
             // 
-            this.UsersTxT.Location = new System.Drawing.Point(12, 84);
+            this.UsersTxT.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UsersTxT.Location = new System.Drawing.Point(10, 79);
             this.UsersTxT.Name = "UsersTxT";
-            this.UsersTxT.Size = new System.Drawing.Size(301, 23);
-            this.UsersTxT.TabIndex = 6;
+            this.UsersTxT.Size = new System.Drawing.Size(318, 22);
+            this.UsersTxT.TabIndex = 2;
             // 
             // SendButton
             // 
-            this.SendButton.Location = new System.Drawing.Point(231, 297);
+            this.SendButton.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.SendButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SendButton.Location = new System.Drawing.Point(241, 252);
             this.SendButton.Name = "SendButton";
-            this.SendButton.Size = new System.Drawing.Size(81, 28);
-            this.SendButton.TabIndex = 7;
+            this.SendButton.Size = new System.Drawing.Size(87, 49);
+            this.SendButton.TabIndex = 4;
             this.SendButton.Text = "Send";
-            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.UseVisualStyleBackColor = false;
             this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
             // 
             // ExitButton
             // 
-            this.ExitButton.Location = new System.Drawing.Point(13, 297);
+            this.ExitButton.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.ExitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExitButton.Location = new System.Drawing.Point(10, 252);
             this.ExitButton.Name = "ExitButton";
-            this.ExitButton.Size = new System.Drawing.Size(81, 28);
-            this.ExitButton.TabIndex = 8;
+            this.ExitButton.Size = new System.Drawing.Size(87, 49);
+            this.ExitButton.TabIndex = 5;
             this.ExitButton.Text = "Exit";
-            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.UseVisualStyleBackColor = false;
             this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(325, 337);
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(340, 313);
             this.Controls.Add(this.ExitButton);
             this.Controls.Add(this.SendButton);
             this.Controls.Add(this.UsersTxT);
@@ -134,10 +145,10 @@ namespace msgGUI
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox ComputerNameTxT;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.RichTextBox MessageTxT;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox UsersTxT;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox MessageTxT;
         private System.Windows.Forms.Button SendButton;
         private System.Windows.Forms.Button ExitButton;
     }
